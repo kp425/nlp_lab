b47e5cc7d82af030a7dfb922de05864b0122bb4a
UchihaMadara <krishna.kp07@gmail.com>
2020-10-04 02:13:21 +0530

    Added positional encoding

--- layers/__init__.py
+++ layers/__init__.py
@@ -1,2 +1,3 @@
 from .multihead_attention import *
 from .transformer_encoder_decoder_layers import *
+from .positional_encoding import *
---
+++ layers/positional_encoding.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+import numpy as np
+
+
+def positional_encoding(pos, d_model):
+
+    def get_angles(pos, i, d_model):
+        angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
+        return pos * angle_rates
+
+    angle_rads = get_angles(np.expand_dims(np.arange(pos), axis = 1), #column_vector
+                          np.expand_dims(np.arange(d_model), axis = 0), #row_vector
+                          d_model)
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+    
+    pos_encoding = angle_rads[np.newaxis, ...]
+    
+    return tf.cast(pos_encoding, dtype=tf.float32)
